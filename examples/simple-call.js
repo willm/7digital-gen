47bbbd3457590da084e138876784b7f5de941664
Will Munn <willmunn@hotmail.com>
2015-06-13 20:14:08 +0100

    moved example to examples folder

--- examples/simple-call.js
+++ examples/simple-call.js
@@ -1,12 +1,15 @@
 'use strict';
 
-let releases = require('./releases');
+let releases = require('../releases');
 let it = main();
+//pass iterator to module.
 let releaseMethods = releases(it);
 
+//generator function to run calls
 function* main () {
     let release = yield releaseMethods.getDetails({releaseId: 12345});
     console.log(release);
 }
 
+//initiate generator
 it.next();

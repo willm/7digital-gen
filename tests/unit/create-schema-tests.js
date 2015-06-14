1c6c4cb614507ed1007ca16e37d5c357963cc93d
Will Munn <willmunn@hotmail.com>
2015-06-14 10:51:39 +0100

    Proxy to provided api

--- tests/unit/create-schema-tests.js
+++ tests/unit/create-schema-tests.js
@@ -4,6 +4,10 @@ let _ = require('lodash');
 
 describe('', () => {
     let api;
+    let sd;
+    let expectedErr = new Error('boom');
+    let expectedRes = {release: {id: '1234'}};
+    let expectedParams = {releaseId: 1234};
     beforeEach(() => {
         let schema = {
             resources: {
@@ -27,7 +31,13 @@ describe('', () => {
                 }
             }
         };
-        api = createSchema(schema);
+        sd = {};
+        sd.Releases = function () {};
+        sd.Releases.prototype.getDetails = function(params, cb){
+            expect(params).to.deep.equal(expectedParams);
+            cb(expectedErr, expectedRes);
+        };
+        api = createSchema(schema, sd);
     });
 
     it('creates an object for every resource', () => {
@@ -39,18 +49,25 @@ describe('', () => {
         expect(api.artist.searchArtists).to.be.a('function');
         expect(api.release.getDetails).to.be.a('function');
     });
-    it('proxies the 7digital api functions', () => {
-        
+    it('proxies the 7digital api functions', (done) => {
+        api.release.getDetails(expectedParams, (err, res) => {
+            expect(err).to.deep.equal(expectedErr);
+            expect(res).to.deep.equal(expectedRes);
+            done();
+        });
     });
 });
 
-function createSchema (schema) {
-    return _.reduce(schema.resources, (api, resource) => {
+function createSchema (schema, sd) {
+    let api = {};
+    _.forIn(schema.resources, (resource, key) => {
         let builtResource = {};
         _.each(resource.actions, (action) => {
-           builtResource[action.methodName] = function () {};
+            builtResource[action.methodName] = function (params, cb) {
+                new sd[key]()[action.methodName](params, cb);
+            };
         });
         api[resource.resource] = builtResource;
-        return api;
-    }, {});
+    });
+    return api;
 }

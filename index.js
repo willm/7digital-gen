314f055215f2c4707075fc854d7b96e83eac5b35
Will Munn <willmunn@hotmail.com>
2016-02-19 12:08:57 +0000

    Add ability to pass api instance

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 'use strict';
-let api = require('7digital-api');
 let schema = require('7digital-api/assets/7digital-api-schema');
 let createSchema = require('./create-schema');
 let createCallback;
 
 
-module.exports = function (it) {
+module.exports = function (it, api) {
+    api = api || require('7digital-api');
     createCallback = function () {
         return function generatorCallback (err, response) {
             if (err) {
